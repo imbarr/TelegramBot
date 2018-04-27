d4a4579e5be768a965f0b3f54fc80413f4d4b43e
imbarr <Ilusha1998>
2018-04-27 17:03:19 +0500

    Finished CommandParser tests part 2

--- src/main/scala/CommandParser.scala
+++ src/main/scala/CommandParser.scala
@@ -7,49 +7,60 @@ import structures.result.{ParseFailureResult, Result}
 import structures.Message.CommandNotFound
 
 import scala.util.Try
+import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
-class CommandParser extends RegexParsers{
+class CommandParser extends RegexParsers {
   val dateFormat = new SimpleDateFormat("HH:mm:ss yy:MM:dd")
 
+  override val whiteSpace: Regex = """[ \r]+""".r
+
   def arg[T](p: Parser[T]): Parser[T] = "(" ~> p <~ ")"
+
   def optional[T](p: Parser[T]): Parser[Option[T]] = p ^^ (x => Some(x)) | """$""".r ^^ (_ => None)
+
   def fullyOptional[T](p: Parser[T]): Parser[Option[T]] = p ^^ (x => Some(x)) | "" ^^ (_ => None)
+
   def optArg[T](p: Parser[T]): Parser[Option[T]] = optional(arg(p))
+
   def toDate(s: String): Option[Date] = Try(dateFormat.parse(s)).toOption
+
   def id(f: Int => Query): Parser[Query] = arg(int) ^^ f
 
-  val string: Parser[String] = """(\(\(|\)\)|[^()])*""".r ^^
-    (s => s.replaceAll("""\(\(""", "(").replaceAll("""\)\)""", ")"))
+  val string: Parser[String] =
+    """(\(\(|\)\)|[^()])*""".r ^^
+      (s => s.replaceAll("""\(\(""", "(").replaceAll("""\)\)""", ")"))
   val int: Parser[Int] = "[0-9]+".r ^^ (p => p.toInt)
   val isAnon: Parser[Boolean] = ("yes" | "no") ^^ (_ == "yes")
   val isVisible: Parser[Boolean] = ("continuous" | "afterstop") ^^ (_ == "continuous")
-  val date: Parser[Date] = "[^()]*".r ^? {case x if toDate(x).isDefined => toDate(x).get}
+  val date: Parser[Date] = "[^()]*".r ^? { case x if toDate(x).isDefined => toDate(x).get }
   val questionType: Parser[QuestionType] =
     "open" ^^ (_ => Open) | "choice" ^^ (_ => Choice) | "multi" ^^ (_ => Multiple)
-  val lines: Parser[List[String]] = ".+".r ^^ (o => o.split("\n").filter(_.isEmpty).toList)
+  val lines: Parser[List[String]] = "(?s).*".r ^^ (o => o.split("\n").filter(_.nonEmpty).toList)
 
   val parsers: Map[String, Parser[Query]] = Map(
-  "/create_poll" ->
-    (arg(string) ~ optArg(isAnon) ~ optArg(isVisible) ~ optArg(date) ~ optArg(date) ^^ {
-    case name ~ anon ~ visible ~ start ~ stop => CreatePollQuery(name, anon, visible, start, stop)}),
-  "/list" -> ("""$""".r ^^ (_ => new ListQuery())),
-  "/delete_poll" -> id(DeletePollQuery),
-  "/start_poll" -> id(StartPollQuery),
-  "/stop_poll" -> id(StopPollQuery),
-  "/result" -> id(ResultQuery),
-  "/begin" -> id(BeginQuery),
-  "/end" -> ("""$""".r ^^ (_ => new EndQuery())),
-  "/view" -> ("""$""".r ^^ (_ => new ViewQuery())),
-  "/add_question" ->
-    (arg(string) ~ fullyOptional(arg(questionType)) ~ optional("\n") ~ lines ^^ {
-    case name ~ qtype ~ _ ~ list => AddQuestionQuery(name, qtype, list)}),
-  "/delete_question" -> id(DeleteQuestionQuery),
-  "/answer" ->
-    (arg(int) ~ arg(string) ^^ (p => AnswerQuestionQuery(p._1, p._2))))
+    "/create_poll" ->
+      (arg(string) ~ optArg(isAnon) ~ optArg(isVisible) ~ optArg(date) ~ optArg(date) ^^ {
+        case name ~ anon ~ visible ~ start ~ stop => CreatePollQuery(name, anon, visible, start, stop)
+      }),
+    "/list" -> ("""$""".r ^^ (_ => new ListQuery())),
+    "/delete_poll" -> id(DeletePollQuery),
+    "/start_poll" -> id(StartPollQuery),
+    "/stop_poll" -> id(StopPollQuery),
+    "/result" -> id(ResultQuery),
+    "/begin" -> id(BeginQuery),
+    "/end" -> ("""$""".r ^^ (_ => new EndQuery())),
+    "/view" -> ("""$""".r ^^ (_ => new ViewQuery())),
+    "/add_question" ->
+      (arg(string) ~ fullyOptional(arg(questionType)) ~ optional("\n") ~ lines ^^ {
+        case name ~ qtype ~ _ ~ list => AddQuestionQuery(name, qtype, list)
+      }),
+    "/delete_question" -> id(DeleteQuestionQuery),
+    "/answer" ->
+      (arg(int) ~ arg(string) ^^ (p => AnswerQuestionQuery(p._1, p._2))))
 
   def parse(s: String): Either[Result, Query] = parsers.get(s.split(" ")(0)) match {
-    case Some(p) => parse(p, s.split(" ").drop(1).mkString(" ")) match{
+    case Some(p) => parse(p, s.split(" ").drop(1).mkString(" ")) match {
       case Success(q, _) => Right(q)
       case NoSuccess(msg, x) => Left(ParseFailureResult(msg, x.pos.column))
     }
--- src/test/scala/CommandParserTests.scala
+++ src/test/scala/CommandParserTests.scala
@@ -2,6 +2,7 @@ import org.scalatest._
 import structures.query._
 import structures.result.{MsgResult, ParseFailureResult}
 import structures.Message.CommandNotFound
+import structures.QuestionType._
 
 class CommandParserTests extends FlatSpec with Matchers {
   val p = new CommandParser()
@@ -16,40 +17,57 @@ class CommandParserTests extends FlatSpec with Matchers {
   def assertWrongFormat(strings: Seq[String]): Unit = for(el <- strings) assertWrongFormat(el)
 
   def assertParsed(cmd: String, q: Query): Assertion = assert(p.parse(cmd).contains(q))
-  def assertParsed(pairs: (String, Query)*): Unit =
+  def assertParsed(pairs: List[(String, Query)]): Unit =
     for(el <- pairs) assertParsed(el._1, el._2)
 
   "Parser" should "parse correct create_poll commands" in {
     val start = "11:40:59 02:01:30"
     val end = "23:40:59 07:01:29"
-    assertParsed(
-      "/create_poll (name)) abc(()))" ->
-        CreatePollQuery("name) abc()"),
-      "/create_poll (n) (yes)" ->
-        CreatePollQuery("n", Some(true)),
-      "/create_poll (n) (no) (afterstop)" ->
-        CreatePollQuery("n", Some(false), Some(false)),
-      "/create_poll (n) (no) (continuous) (" + start + ")" ->
+    assertParsed(List(
+      "(name)) abc(()))" -> CreatePollQuery("name) abc()"),
+      "(n) (yes)" -> CreatePollQuery("n", Some(true)),
+      "(n) (no) (afterstop)" -> CreatePollQuery("n", Some(false), Some(false)),
+      "(n) (no) (continuous) (" + start + ")" ->
         CreatePollQuery("n", Some(false), Some(true), Some(p.dateFormat.parse(start))),
-      "/create_poll (n) (no) (continuous) (" + start + ") (" + end + ")" ->
+      "(n) (no) (continuous) (" + start + ") (" + end + ")" ->
         CreatePollQuery("n", Some(false), Some(true),
-          Some(p.dateFormat.parse(start)), Some(p.dateFormat.parse(end))))
+          Some(p.dateFormat.parse(start)), Some(p.dateFormat.parse(end)))
+    ).map(p => ("/create_poll " + p._1, p._2)))
   }
 
   it should "not parse invalid create_poll commands" in assertWrongFormat(List(
     "(((name))", "(n) (some)", "(n) (continuous)",
     "(n) (no) (g)", "(n) (no) (afterstop) (time) (time)").map("/create_poll " + _))
 
-  it should "parse list command" in isInstance[ListQuery](p.parse("/list"))
+  it should "parse correct add_question commands" in assertParsed(List(
+    "(name)\n1\n2\n3" -> AddQuestionQuery("name", options=List("1", "2", "3")),
+    "(n) (open)" -> AddQuestionQuery("n", Some(Open)),
+    "(n) (choice)\n1" -> AddQuestionQuery("n", Some(Choice), List("1")),
+    "(n) (multi)\n1\nabc" -> AddQuestionQuery("n", Some(Multiple), List("1", "abc")),
+  ).map(p => ("/add_question " + p._1, p._2)))
 
-  it should "parse other correct poll commands" in assertParsed(
+  it should "not parse invalid add_question commands" in{
+    assertWrongFormat(List("/add_question (n) (asas)", "/add_question (n) (open) (asas)"))
+  }
+
+  it should "parse commands with no arguments" in {
+    isInstance[ListQuery](p.parse("/list"))
+    isInstance[EndQuery](p.parse("/end"))
+    isInstance[ViewQuery](p.parse("/view"))
+  }
+
+  it should "parse other correct poll commands" in assertParsed(List(
       "/delete_poll (99999)" -> DeletePollQuery(99999),
       "/start_poll (0)" -> StartPollQuery(0),
       "/stop_poll (10)" -> StopPollQuery(10),
-      "/result (0)" -> ResultQuery(0))
+      "/result (0)" -> ResultQuery(0),
+      "/begin (76)" -> BeginQuery(76),
+      "/delete_question (3)" -> DeleteQuestionQuery(3),
+      "/answer (9) (abcd)" -> AnswerQuestionQuery(9, "abcd")))
 
   it should "not parse invalid poll commands" in {
-    val cmds: List[String] = List("/delete_poll", "/start_poll", "/stop_poll", "/result")
+    val cmds: List[String] = List("/delete_poll", "/start_poll",
+      "/stop_poll", "/result", "/begin", "/delete_question")
     val args: List[String] = List("(-1)", "45", "(ab)")
     for{
       c <- cmds
@@ -59,6 +77,4 @@ class CommandParserTests extends FlatSpec with Matchers {
 
   it should "not parse unrecognized commands" in
     assert(p.parse("/some_not_implemented_command").left.exists(_ == MsgResult(CommandNotFound)))
-
-  it should "not parse not command strings" in assertWrongFormat("result")
 }
